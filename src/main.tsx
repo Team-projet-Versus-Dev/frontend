717dcc8cd5878306fd96d0b34c0a993cbbdaa0bc
paulpierrard <paul.pierrard77860@gmail.com>
2025-11-26 16:45:30 +0100

    update for the footer and the header

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import './component/header.tsx'
+import Header from './component/header.tsx'
+import Footer from './component/footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Header />
     <App />
+    <Footer />
   </StrictMode>,
 )
